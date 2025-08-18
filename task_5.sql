67439ec6142b4ff65304f0e4d325f9b2d12a1637
sosna <gemechusosi@gmail.com>
2025-08-19 00:19:04 +0300

    add task_3.sql edited

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,4 @@
 -- task_5.sql
 
--- Insert a single customer into the customers table
 INSERT INTO customers (customer_id, first_name, last_name, email, address)
 VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
