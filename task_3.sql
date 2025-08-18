46d578c7d894bcccecc6e675f256b1f67c7cfe03
sosna <gemechusosi@gmail.com>
2025-08-19 00:02:40 +0300

    edited version

--- task_3.sql
+++ task_3.sql
@@ -1,11 +1,6 @@
 -- task_3.sql
 
--- List all tables in the current database
-SHOW TABLES;
+USE alx_book_store;
 
--- If you prefer to list using information_schema (works regardless of current database selection)
-SELECT TABLE_SCHEMA,
-       TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store'
-ORDER BY TABLE_NAME;
+-- List all tables in the database
+SHOW TABLES;
