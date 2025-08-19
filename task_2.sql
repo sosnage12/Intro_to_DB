af5a833c2727314f36531b1507aa2faa501998ee
sosna <gemechusosi@gmail.com>
2025-08-19 11:56:08 +0300

    add edited task_2.sql to git hub

--- task_2.sql
+++ task_2.sql
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    phone VARCHAR(15)
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    address TEXT
 );
 
 -- Create Orders table
