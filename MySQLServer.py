0e106c41d68144e3d63262e740c8759c9a5cda34
sosna <gemechusosi@gmail.com>
2025-08-24 16:14:34 +0300

    push edited #!/usr/bin/python3

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,6 @@ Script to create database 'alx_book_store' in MySQL server
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -22,7 +21,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:  # <-- This matches the check
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
